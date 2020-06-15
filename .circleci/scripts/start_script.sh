70d262a0e6fa49c541e473353808aca3015f36c9
koga <koga@wh-plus.com>
2020-06-15 09:09:50 +0900

    CIコンテナで展開するソースコードの場所をコンテナの作業ディレクトリに変更

--- .circleci/scripts/start_script.sh
+++ .circleci/scripts/start_script.sh
@@ -4,7 +4,7 @@ cp -af ./.circleci/docker/docker-compose74.yaml docker-compose.yml
 DOCKER_COMPOSE="docker-compose -f docker-compose.yml"
 ( $DOCKER_COMPOSE build && $DOCKER_COMPOSE up -d && $DOCKER_COMPOSE rm )
 
-docker cp /home/circleci/src laravel:/var/app
-docker exec -it laravel php /var/app/composer.phar dump-autoload
-docker exec -it laravel php /var/app/composer.phar install --no-interaction
-docker exec -it laravel php /var/app/vendor/bin/phpunit
+docker cp /home/circleci/src laravel:/var/www
+docker exec -it laravel php /var/www/composer.phar install --no-interaction --working-dir=/var/www
+docker exec -it laravel php /var/www/composer.phar dump-autoload --working-dir=/var/www
+docker exec -it laravel php /var/www/vendor/bin/phpunit .
