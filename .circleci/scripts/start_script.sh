ef617350f26428e4d40212291a194bd56381b0fe
koga <koga@wh-plus.com>
2020-06-17 09:19:32 +0900

    ソースコードが配置されるパス指定が間違っていたので修正

--- .circleci/scripts/start_script.sh
+++ .circleci/scripts/start_script.sh
@@ -4,7 +4,6 @@ cp -af ./.circleci/docker/docker-compose74.yaml docker-compose.yml
 DOCKER_COMPOSE="docker-compose -f docker-compose.yml"
 ( $DOCKER_COMPOSE build && $DOCKER_COMPOSE up -d && $DOCKER_COMPOSE rm )
 
-docker cp /home/circleci/src laravel:/var/www
-docker exec -it laravel php /var/www/composer.phar install --no-interaction --working-dir=/var/www
-docker exec -it laravel php /var/www/composer.phar dump-autoload --working-dir=/var/www
-docker exec -it laravel php /var/www/vendor/bin/phpunit .
+docker cp /home/circleci/src laravel:/var/www/
+docker exec -it laravel php /var/www/src/composer.phar install --no-interaction --working-dir=/var/www/src
+docker exec -it laravel php /var/www/src/composer.phar dump-autoload --working-dir=/var/www/src
